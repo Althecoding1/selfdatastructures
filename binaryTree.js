39103705674c92ab234d2a87c180a529f53b6e0f
Arthur Livingston <Artielivingston1@gmail.com>
2017-01-10 17:42:49 -0800

    completed binaryTree

--- binaryTree.js
+++ binaryTree.js
@@ -13,3 +13,35 @@ let prettyPrint = (curr) => {
     prettyPrint(curr.right);
   }
 };
+
+const append = (curr, data) => {
+  if(data <= curr.data) {
+    if(curr.left === null) {
+      curr.left = new Node(data, null, null);
+    } else {
+      append(curr.left, data);
+    }
+  } else {
+    if(curr.right === null) {
+      curr.right = new Node(data, null, null);
+    } else {
+      append(curr.right, data);
+    }
+  }
+}
+
+function BinaryTree() {
+  this.root = new Node(null, null, null);
+};
+
+BinaryTree.prototype.prettyPrint = function(data) {
+
+};
+
+BinaryTree.prototype.append = function(data) {
+
+};
+
+BinaryTree.prototype.remove = function() {
+
+};
